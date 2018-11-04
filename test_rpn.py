7a57fcdbda1fe51af7927f42f18e769077e3aef7
harsparc <harsparc@umich.edu>
2018-11-04 11:23:47 -0500

    Added test for multiply, divide, expo

--- test_rpn.py
+++ test_rpn.py
@@ -14,4 +14,15 @@ class TestBasics(unittest.TestCase):
 	def test_toomany(self):
 		with self.assertRaises(ValueError):
 			result = rpn.calculate("1 2 3 +")
-		                                     
+
+	def test_multiply(self):
+		result = rpn.calculate("3 5 *")
+		self.assertEqual(15, result)
+
+	def test_divide(self):
+		result = rpn.calculate("16 4 /")
+		self.assertEqual(4, result)
+
+	def test_expo(self):
+		result = rpn.calculate("2 2 ^")
+		self.assertEqual(4, result)		
